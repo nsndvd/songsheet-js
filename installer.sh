160d78304be33314bd4fc80eccb6b25f840ffe52
jannes <jannes.hp@t-online.de>
2017-11-24 13:20:09 +0100

    fix bad simple error

--- installer.sh
+++ installer.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # merge and min files
-    # merge files
-    cat ./src/header.js ./src/block.js ./src/layouts/layout.js ./src/line.js ./src/parser.js ./src/song.js ./src/songsheetGen.js ./src/utils.js ./src/layouts/simplestLayout.js ./src/fPDF.js ./src/pdfmake/build/pdfmake.js ./src/layouts/vfs_fonts.js ./src/images.js > ./songsheet.js
-    # minify file
-    uglifyjs songsheet.js > songsheet.min.js
+    ./zip.sh
 
 # generate docs
     # clone wiki git to tmp
