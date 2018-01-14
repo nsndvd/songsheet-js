4984481ddf0b003c1a95fb98b1d603f442988921
jannes <jannes.hp@t-online.de>
2018-01-14 09:48:37 +0100

    add fixes

--- index.php
+++ index.php
@@ -9,37 +9,20 @@
     <title>Test</title>
 </head>
 <body>
+    <button onclick="run()">Auf gut Glück!</button>
+    <textarea id="input">
+        [title: Blessed Be Your Name (Intro - XX); bpm: 80]
+    </textarea>
     <script src="songsheet.js"></script>
     <script>
+
+        function run() {
             let gen = new SongsheetGen();
-            let txt = `[TITLE:How great is our god]
-
-[ORDER:Intro, Str 1]
-
-[block: Intro]
-[G] [Em7] [C] [D] [G]
-
-[Block : Verse 2]
-[Em7] Back beat, the [G]word is on the street
-That the fire in your heart is out,
-
-[BLOCK: Ref]
-How [G]great is our God, sing with me       | test1     ;   test2; test3; test4; test5 | always
-How [Em7]great is our God, and all will see
-How [Cmaj7]great, how [D]great is our [G]God
-
-[BLOCK: Str 1]
-[G]Age to age He stands, and [Em7]time is in His hands
-Beginning and the [C]end, Beginning and the [D]end
-The Godhead Three in One, Father Spirit Son
-The [C]Lion and the Lamb, The [D]Lion and the Lamb
-
-[bLoCk: Bridge]
-[G]Name above all names, [Em7]Worthy of our praise
-My [Cmaj7]heart will sing, How [D]great is our [G]God`;
+            let txt = document.getElementById('input').value;
             gen.add_song(txt, {"table": true});
-            let res = gen.gen(true);
+            let res = gen.gen(false);
             document.body.innerHTML = 'var dd = ' + JSON.stringify(res[0], null, 2) + ';';
+        }
     </script>
 </body>
 </html>
